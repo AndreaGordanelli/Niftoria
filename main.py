6b0ca56f4f8fc2996f2338c288b2706b0f6ae099
Andrea Gordanelli <72037340+AndreaGordanelli@users.noreply.github.com>
2022-02-21 01:20:36 +0100

    Added Title and args

---
+++ main.py
@@ -0,0 +1,27 @@
+import argparse
+title = """\
+ ███▄    █  ██▓  █████▒▄▄▄█████▓ ▒█████   ██▀███   ██▓ ▄▄▄      
+ ██ ▀█   █ ▓██▒▓██   ▒ ▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒▓██▒▒████▄    
+▓██  ▀█ ██▒▒██▒▒████ ░ ▒ ▓██░ ▒░▒██░  ██▒▓██ ░▄█ ▒▒██▒▒██  ▀█▄  
+▓██▒  ▐▌██▒░██░░▓█▒  ░ ░ ▓██▓ ░ ▒██   ██░▒██▀▀█▄  ░██░░██▄▄▄▄██ 
+▒██░   ▓██░░██░░▒█░      ▒██▒ ░ ░ ████▓▒░░██▓ ▒██▒░██░ ▓█   ▓██▒
+░ ▒░   ▒ ▒ ░▓   ▒ ░      ▒ ░░   ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░░▓   ▒▒   ▓▒█░
+░ ░░   ░ ▒░ ▒ ░ ░          ░      ░ ▒ ▒░   ░▒ ░ ▒░ ▒ ░  ▒   ▒▒ ░
+   ░   ░ ░  ▒ ░ ░ ░      ░      ░ ░ ░ ▒    ░░   ░  ▒ ░  ░   ▒   
+         ░  ░                       ░ ░     ░      ░        ░  ░
+                                                                
+"""
+# Initialize parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-H", "--host", help = "Attacking IP")
+parser.add_argument("-P", "--port", help = "Attacking port")
+parser.parse_args()
+
+
+
+if __name__ == "__main__":
+    print(title)
+    args = parser.parse_args()
+ 
+    if args.host and args.port:
+        print(f"IP della macchina attaccante: {args.host}:{args.port}")
